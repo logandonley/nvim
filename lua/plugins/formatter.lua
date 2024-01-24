0d6f693274af29f25fa3cb05c1e4f52a4ac48262
Logan Donley <hello@logandonley.com>
2024-01-23 20:00:28 -0500

    feat: adding format on save

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -4,6 +4,7 @@ return {
     local null_ls = require("null-ls")
     local formatting = null_ls.builtins.formatting
     local diagnostics = null_ls.builtins.diagnostics
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
     local sources = {}
     sources[#sources + 1] = formatting.black
@@ -20,9 +21,20 @@ return {
     sources[#sources + 1] = formatting.isort
     sources[#sources + 1] = diagnostics.flake8
 
-
     null_ls.setup({
       sources = sources,
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ async = false })
+            end,
+          })
+        end
+      end,
     })
   end,
 }
